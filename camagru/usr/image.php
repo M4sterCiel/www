004a31b41e994ba31aa5939b04d5cca9929704d2
raphael <raphael.allemand@live.fr>
2019-05-02 17:33:29 +0200

    improving php camera

--- camagru/usr/image.php
+++ camagru/usr/image.php
@@ -3,6 +3,8 @@ require "../model.php";
 
 $path_layer = "../img/" . $_POST['layer'];
 $layer = imagecreatefrompng($path_layer);
+
+
 $img = $_POST['image'];
 $img = preg_replace('/ /', '+', $img);
 $path = "../pictures/";
@@ -12,13 +14,14 @@ $image_type = $image_type_aux[1];
 $image_en_base64 = base64_decode($image_parts[1]);
 $file = $path . uniqid() . ".png";
 file_put_contents($file, $image_en_base64);
+
+
 $img_src = imagecreatefrompng($file);
 $size_layer = getimagesize($path_layer);
 $size_img = getimagesize($file);
-echo $size_img[0];
-echo "\n";
-echo $size_img[1];
-imagecopyresampled($img_src, $layer, 0, 0, 0, 0, $size_img[0], $size_img[1], $size_layer[0], $size_layer[1]);
+
+imagecopy($img_src, $layer, 0, 0, 0, 0, $size_layer[0], $size_layer[1]);
+/* imagecopyresampled($img_src, $layer, 0, 0, 0, 0, $size_img[0], $size_img[1], $size_layer[0], $size_layer[1]); */
 imagepng($img_src, $file);
 
 
