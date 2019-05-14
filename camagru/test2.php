cabfe6b25604347dc7a49ce49f101a664355f459
M4sterCiel <43251804+M4sterCiel@users.noreply.github.com>
2019-05-14 15:41:04 +0200

    Delete test2.php

--- camagru/test2.php
+++
@@ -1,5 +0,0 @@
-<video id="video"></video>
-<button id="startbutton">Prendre une photo</button>
-<canvas id="canvas"></canvas>
-<img src="http://placekitten.com/g/320/261" id="photo" alt="photo">
-<script type="text/javascript" src="js.js"></script>
